d9dec74414fb0143e0b932ae4fec58e2d56aef94
abhishekkuntal081 <abhishekkuntal081@gmail.com>
2026-01-21 10:15:26 +0530

    [Abhishek] Added File

---
+++ programming-constructs/Division.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Division
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a=sc.nextInt();
+		 if(a%5==0)
+		 {
+			 System.out.println("odd "+a);
+		 }
+		 else{
+			 System.out.println("even" +a);
+	}
+	}
+}
